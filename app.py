f35769185e001f9afe4c4c39d63ee21fe7d67676
jchartsma <jchartsma2026@gmail.com>
2026-07-18 11:41:11 +0900

    Add files via upload

--- app.py
+++ app.py
@@ -383,7 +383,7 @@ def index():
 
     <div id="interval-right">
         <a id="notice-link" class="pc-like-button">注意事項▼</a>
-        <a id="pc-link" href="https://japan-stock-chart-pc.onrender.com/">PC画面</a>
+        <a id="pc-link" href="https://j-chart-pc.onrender.com/">PC画面</a>
     </div>
 </div>
 
